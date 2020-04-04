ee8ab154ad5fb885dc54b17e3eec791d61949fa7
fishercht1995 <fuyuqi1995@gmail.com>
2020-04-04 15:13:47 -0400

    install

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-./deploy.sh
-./init.sh
+bash /kubernetes-examples/deploy.sh
+bash /kubernetes-examples/init.sh
 echo "new token create"
-kubeadm token create --print-join-command > token.sh
+kubeadm token create --print-join-command > /kubernetes-examples/token.sh
+chmod 777 /kubernetes-examples/token.sh
 
 cat workers | while read line
 do
     if [ "$line" = "-" ]; then
         echo "Skip $line"
     else
-        ssh root@$line -n "rm -rf /kubernetes-install/ && mkdir /kubernetes-installer/"
+        ssh root@$line -n "rm -rf /kubernetes-examples/ && mkdir /kubernetes-examples/"
         echo "Copy data to $line"
-        scp  /mapreduce-test/setup.py root@$line:/mapreduce-test/ && scp /mapreduce-test/manager root@$line:/mapreduce-test/ && scp /mapreduce-test/workers root@$line:/mapreduce-test/
+        scp  /kubernetes-examples/deploy.sh root@$line:/kubernetes-examples/ && scp /kubernetes-examples/token.sh root@$line:/mapreduce-test/
         echo "Setup $line"
-        ssh root@$line -n "cd /mapreduce-test/ && python3 setup.py && ntpdate time.nist.gov"
+        ssh root@$line -n "cd /mapreduce-test/ && sudo ./deploy.sh && sudo ./token.sh"
         echo "Finished config node $line"
     fi
 done
